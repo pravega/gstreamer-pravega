c31ee85b1e3e2479c4b606a44bc752ec4585df04
Claudio Fahey <claudio.fahey@dell.com>
2021-04-22 21:07:16 +0000

    pravega-to-screen.sh now uses PRAVEGA_STREAM

--- scripts/pravega-to-screen.sh
+++ scripts/pravega-to-screen.sh
@@ -9,7 +9,7 @@ export GST_DEBUG="pravegasrc:INFO,mpegtsbase:4,mpegtspacketizer:4,GST_TRACER:7"
 export RUST_BACKTRACE=1
 PRAVEGA_CONTROLLER_URI=${PRAVEGA_CONTROLLER_URI:-127.0.0.1:9090}
 PRAVEGA_SCOPE=${PRAVEGA_SCOPE:-examples}
-STREAM=${STREAM:-test1}
+PRAVEGA_STREAM=${PRAVEGA_STREAM:-${STREAM:-test1}}
 ALLOW_CREATE_SCOPE=${ALLOW_CREATE_SCOPE:-true}
 export GST_DEBUG_DUMP_DOT_DIR=/tmp/gst-dot/pravega-to-screen
 mkdir -p ${GST_DEBUG_DUMP_DOT_DIR}
@@ -18,8 +18,12 @@ gst-launch-1.0 \
 -v \
 pravegasrc \
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
   stream=${PRAVEGA_SCOPE}/${STREAM} \
+=======
+  stream=${PRAVEGA_SCOPE}/${PRAVEGA_STREAM} \
+>>>>>>> pravega-to-screen.sh now uses PRAVEGA_STREAM
   controller=${PRAVEGA_CONTROLLER_URI} \
 >>>>>>> Change scripts to use PRAVEGA_CONTROLLER_URI and PRAVEGA_SCOPE
   allow-create-scope=${ALLOW_CREATE_SCOPE} \
@@ -29,6 +33,6 @@ pravegasrc \
   $* \
 ! decodebin \
 ! videoconvert \
-! textoverlay "text=from ${STREAM}" valignment=baseline halignment=right "font-desc=Sans 24px" shaded-background=true \
+! textoverlay "text=from ${PRAVEGA_STREAM}" valignment=baseline halignment=right "font-desc=Sans 24px" shaded-background=true \
 ! autovideosink sync=false \
 |& tee /tmp/pravega-to-screen.log
