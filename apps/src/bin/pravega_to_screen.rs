8f5cd35da198bdb6cbaad168bf0690cf3d9a489e
Luis Liu <luis_liu@dell.com>
2021-04-25 09:35:05 +0800

    add license header for all the files
    Signed-off-by: Luis Liu <luis_liu@dell.com>

--- apps/src/bin/pravega_to_screen.rs
+++ apps/src/bin/pravega_to_screen.rs
@@ -1,3 +1,13 @@
+//
+// Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+
 // Based on gstreamer-rs/tutorials/src/bin/basic-tutorial-4.rs.
 use gst::prelude::*;
 use gst::MessageView;
--- deepstream/python_apps/deepstream-pravega-demos/rtsp-camera-to-object-detection-to-pravega.py
+++ deepstream/python_apps/deepstream-pravega-demos/rtsp-camera-to-object-detection-to-pravega.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+
+#
+# Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
 #
 # Capture from RTSP camera, detect objects, write video data and metadata to a Pravega stream.
 # Note: This method has some drawbacks. See pravega-to-object-detection-to-pravega.py for the recommended method.
