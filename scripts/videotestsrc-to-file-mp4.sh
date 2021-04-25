0c3706424268fec205dae9a238a8dd8c8bae5cea
Claudio Fahey <claudio.fahey@dell.com>
2021-04-25 18:50:50 +0000

    Add videotestsrc-to-file-mp4.sh

---
+++ scripts/videotestsrc-to-file-mp4.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ex
+ROOT_DIR=$(readlink -f $(dirname $0)/..)
+
+export GST_DEBUG=qtmux:LOG
+
+gst-launch-1.0 \
+-v \
+  videotestsrc name=src is-live=true do-timestamp=true num-buffers=300 \
+! video/x-raw,width=160,height=120,framerate=30/1 \
+! videoconvert \
+! clockoverlay "font-desc=Sans, 48" "time-format=%F %T" \
+! x264enc key-int-max=60 tune=zerolatency \
+! mp4mux streamable=true fragment-duration=100 \
+! identity silent=false \
+! filesink location=${HOME}/test30.mp4 \
+|& tee /tmp/videotestsrc-to-file-mp4.log
