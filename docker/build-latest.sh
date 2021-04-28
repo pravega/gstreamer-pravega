18d033b0d9f93946428e5cfc584bca9ca7cebcdd
John Daniel Maguire <john.maguire@emc.com>
2021-04-28 09:16:34 -0300

    Refactor dockerfiles

--- docker/build-latest.sh
+++ docker/build-latest.sh
@@ -14,17 +14,10 @@ set -ex
 
 # Make sure to always have fresh base image
 docker pull ubuntu:20.10
-# Install dev dependencies
-docker build -t pravega/gstreamer:dev-dependencies -f Dockerfile-dev-dependencies .
-# Download source code
-docker build -t pravega/gstreamer:dev-downloaded -f Dockerfile-dev-downloaded .
-# Build dev image with source code included
-docker build -t pravega/gstreamer:latest-dev-with-source -f Dockerfile-dev-with-source .
-# Build dev image with just binaries
-docker build -t pravega/gstreamer:latest-dev -f Dockerfile-dev .
-# Build base production image with necessary dependencies
-docker build -t pravega/gstreamer:prod-base -f Dockerfile-prod-base .
+docker build -t pravega/gstreamer:dev-downloaded -f dev.Dockerfile --target download .
+docker build -t pravega/gstreamer:latest-dev -f dev.Dockerfile .
+
 # Build production image optimized binaries and no debug symbols (-O3 LTO)
-docker build -t pravega/gstreamer:latest-prod -f Dockerfile-prod .
+docker build -t pravega/gstreamer:latest-prod -f prod.Dockerfile --target prod .
 # Build production image optimized binaries and debug symbols
-docker build -t pravega/gstreamer:latest-prod-dbg -f Dockerfile-prod-dbg .
+docker build -t pravega/gstreamer:latest-prod-dbg -f prod.Dockerfile --target debug-prod .
