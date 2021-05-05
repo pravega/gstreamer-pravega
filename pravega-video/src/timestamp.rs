c4b4f4926be3d10821ed617687b3ec8584abf2c9
Claudio Fahey <claudio.fahey@dell.com>
2021-05-05 07:03:10 +0000

    pravega-video: Add operators to PravegaTimestamp. Add TimeDelta.
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- pravega-video/src/timestamp.rs
+++ pravega-video/src/timestamp.rs
@@ -11,10 +11,11 @@
 use anyhow;
 use std::convert::{TryInto, TryFrom};
 use std::fmt;
+use std::ops::{Add, Mul, Sub, Div};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Default)]
 /// This stores the number of nanoseconds since the TAI epoch 1970-01-01 00:00 TAI (International Atomic Time).
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Default)]
 pub struct PravegaTimestamp(pub Option<u64>);
 
 impl PravegaTimestamp {
@@ -220,5 +221,257 @@ impl fmt::Debug for PravegaTimestamp {
                 },
             None => f.write_str("None"),
         }
+    }
+}
+
+impl Add for PravegaTimestamp {
+    type Output = PravegaTimestamp;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self.0, rhs.0) {
+            (Some(this), Some(rhs)) => Self(Some(this + rhs)),
+            _ => Self(None),
+        }
+    }
+}
+
+/// This allows expressions such as "PravegaTimestamp::now() - PravegaTimestamp::now()".
+impl Sub for PravegaTimestamp {
+    type Output = TimeDelta;
+
+    fn sub(self, rhs: PravegaTimestamp) -> Self::Output {
+        match (self.0, rhs.0) {
+            (Some(this), Some(rhs)) => TimeDelta(Some(this as i128 - rhs as i128)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+/// This allows expressions such as "PravegaTimestamp::now() + Duration::from_nanos(1_000_500_000)".
+impl Add<Duration> for PravegaTimestamp {
+    type Output = PravegaTimestamp;
+
+    fn add(self, rhs: Duration) -> Self::Output {
+        match self.0 {
+            Some(this) => Self(Some(this + rhs.as_nanos() as u64)),
+            _ => Self(None),
+        }
+    }
+}
+
+/// A time delta (difference), represented as a positive or negative number of nanoseconds.
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Default)]
+pub struct TimeDelta(pub Option<i128>);
+
+impl TimeDelta {
+    /// Convert to format +h:mm:ss.fffffffff
+    /// Based on https://gstreamer.freedesktop.org/documentation/gstreamer/gstclock.html?gi-language=c#GST_STIME_ARGS.
+    pub fn to_hms(&self) -> Option<String> {
+        match self.0 {
+            Some(ns) => {
+                let sign = if ns >= 0 { "+" } else { "-" };
+                let ns = i128::abs(ns);
+                const SEC: i128 = 1_000_000_000;
+                let h = ns / (SEC * 60 * 60);
+                let mm = (ns / (SEC * 60)) % 60;
+                let ss = (ns / SEC) % 60;
+                let f = ns % SEC;
+                Some(format!("{}{}:{:02}:{:02}.{:09}", sign, h, mm, ss, f))
+                },
+            None => None,
+        }
+    }
+}
+
+impl Add for TimeDelta {
+    type Output = TimeDelta;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self.0, rhs.0) {
+            (Some(this), Some(rhs)) => Self(Some(this + rhs)),
+            _ => Self(None),
+        }
+    }
+}
+
+impl Sub for TimeDelta {
+    type Output = TimeDelta;
+
+    fn sub(self, rhs: TimeDelta) -> Self::Output {
+        match (self.0, rhs.0) {
+            (Some(this), Some(rhs)) => TimeDelta(Some(this - rhs)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+/// This allows expressions such as "10 * SECOND".
+impl Mul<TimeDelta> for i128 {
+    type Output = TimeDelta;
+
+    fn mul(self, rhs: TimeDelta) -> Self::Output {
+        match rhs.0 {
+            Some(rhs) => TimeDelta(Some(self * rhs)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+impl Mul<TimeDelta> for u128 {
+    type Output = TimeDelta;
+
+    fn mul(self, rhs: TimeDelta) -> Self::Output {
+        match rhs.0 {
+            Some(rhs) => TimeDelta(Some(self as i128 * rhs)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+impl Mul<TimeDelta> for u64 {
+    type Output = TimeDelta;
+
+    fn mul(self, rhs: TimeDelta) -> Self::Output {
+        match rhs.0 {
+            Some(rhs) => TimeDelta(Some(self as i128 * rhs)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+impl Mul<TimeDelta> for i32 {
+    type Output = TimeDelta;
+
+    fn mul(self, rhs: TimeDelta) -> Self::Output {
+        match rhs.0 {
+            Some(rhs) => TimeDelta(Some(self as i128 * rhs)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+/// This allows expressions such as "SECOND / 10".
+impl Div<i128> for TimeDelta {
+    type Output = TimeDelta;
+
+    fn div(self, rhs: i128) -> TimeDelta {
+        match self.0 {
+            Some(this) => TimeDelta(Some(this / rhs)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+impl Div<u64> for TimeDelta {
+    type Output = TimeDelta;
+
+    fn div(self, rhs: u64) -> TimeDelta {
+        match self.0 {
+            Some(this) => TimeDelta(Some(this / rhs as i128)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+impl Div<i32> for TimeDelta {
+    type Output = TimeDelta;
+
+    fn div(self, rhs: i32) -> TimeDelta {
+        match self.0 {
+            Some(this) => TimeDelta(Some(this / rhs as i128)),
+            _ => TimeDelta(None),
+        }
+    }
+}
+
+/// This allows expressions such as "PravegaTimestamp::now() + SECOND".
+impl Add<TimeDelta> for PravegaTimestamp {
+    type Output = PravegaTimestamp;
+
+    fn add(self, rhs: TimeDelta) -> Self::Output {
+        match (self.0, rhs.0) {
+            (Some(this), Some(rhs)) => Self(Some((this as i128 + rhs) as u64)),
+            _ => Self(None),
+        }
+    }
+}
+
+/// This allows expressions such as "PravegaTimestamp::now() - SECOND".
+impl Sub<TimeDelta> for PravegaTimestamp {
+    type Output = PravegaTimestamp;
+
+    fn sub(self, rhs: TimeDelta) -> Self::Output {
+        match (self.0, rhs.0) {
+            (Some(this), Some(rhs)) => Self(Some((this as i128 - rhs) as u64)),
+            _ => Self(None),
+        }
+    }
+}
+
+impl fmt::Display for TimeDelta {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        f.write_str(&self.to_hms().unwrap_or("None".to_owned()))
+    }
+}
+
+impl fmt::Debug for TimeDelta {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        f.write_str(&self.to_hms().unwrap_or("None".to_owned()))
+    }
+}
+
+pub const DAY: TimeDelta = TimeDelta(Some(24 * 60 * 60 * 1_000_000_000));
+pub const HOUR: TimeDelta = TimeDelta(Some(60 * 60 * 1_000_000_000));
+pub const MINUTE: TimeDelta = TimeDelta(Some(60 * 1_000_000_000));
+pub const SECOND: TimeDelta = TimeDelta(Some(1_000_000_000));
+pub const MSECOND: TimeDelta = TimeDelta(Some(1_000_000));
+pub const USECOND: TimeDelta = TimeDelta(Some(1_000));
+pub const NSECOND: TimeDelta = TimeDelta(Some(1));
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_pravega_timestamp() {
+        let s1 = "2001-02-03T04:00:00.000000000Z";
+        let pt1 = PravegaTimestamp::try_from(Some(s1)).unwrap();
+        println!("s1 ={}", s1);
+        println!("pt1={}", pt1);
+        println!("pt1={:?}", pt1);
+        let s2 = pt1.to_iso_8601().unwrap();
+        assert_eq!(s1, s2);
+
+        let dpt2 = PravegaTimestamp::from_nanoseconds(Some(1_000_000_000));
+        println!("dpt2={}", dpt2);
+        println!("dpt2={:?}", dpt2);
+        let pt3 = pt1 + dpt2;
+        println!("pt3={:?}", pt3);
+        assert_eq!(pt3.to_iso_8601().unwrap(), "2001-02-03T04:00:01.000000000Z");
+
+        let dur4 = Duration::from_nanos(1_000_500_000);
+        println!("dur4={:?}", dur4);
+        let pt5 = pt1 + dur4;
+        println!("pt5={:?}", pt5);
+        assert_eq!(pt5.to_iso_8601().unwrap(), "2001-02-03T04:00:01.000500000Z");
+
+        let delta6 = 3 * SECOND;
+        println!("delta6={}", delta6);
+        let pt7: PravegaTimestamp = pt1 + delta6;
+        println!("pt7={:?}", pt7);
+        assert_eq!(pt7.to_iso_8601().unwrap(), "2001-02-03T04:00:03.000000000Z");
+
+        let delta8 = pt7 - pt1;
+        assert_eq!(delta8.to_hms().unwrap(), "+0:00:03.000000000");
+        assert_eq!(delta8, delta6);
+
+        let delta9 = pt1 - pt7;
+        assert_eq!(delta9.to_hms().unwrap(), "-0:00:03.000000000");
+
+        let delta10 = pt1 - pt1;
+        assert_eq!(delta10.to_hms().unwrap(), "+0:00:00.000000000");
+
+        let delta11: TimeDelta = delta8 / 10;
+        assert_eq!(delta11.to_hms().unwrap(), "+0:00:00.300000000");
     }
 }
