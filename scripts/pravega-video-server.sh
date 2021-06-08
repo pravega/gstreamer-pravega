aae212ba687ca86c9946e0ab752f4bc109ed843e
Claudio Fahey <claudio.fahey@dell.com>
2021-06-08 22:21:17 +0000

    Fix pravega-video-server.sh
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- scripts/pravega-video-server.sh
+++ scripts/pravega-video-server.sh
@@ -15,7 +15,6 @@ ROOT_DIR=$(readlink -f $(dirname $0)/..)
 # log level can be INFO or LOG (verbose)
 export RUST_LOG=info
 export RUST_BACKTRACE=1
-PRAVEGA_CONTROLLER_URI=${PRAVEGA_CONTROLLER_URI:-127.0.0.1:9090}
 pushd ${ROOT_DIR}/pravega-video-server
 cargo run -- $* \
 |& tee /tmp/pravega-video-server.log
