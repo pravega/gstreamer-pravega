7d2ebfa0276cee84c95650d6a506bf37d613af61
Claudio Fahey <claudio.fahey@dell.com>
2021-08-04 23:20:20 +0000

    Add gstpravega Python library with HealthCheckServer and other utilities
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

---
+++ python_apps/lib/gstpravega/health_check.py
@@ -0,0 +1,108 @@
+#
+# Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
+import logging
+import time
+from threading import Thread
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from .util import str2bool
+import gi
+from gi.repository import GObject, Gst
+
+
+class HealthCheckServer():
+    @staticmethod
+    def add_arguments(parser):
+        """Add arguments to an argparse instance"""
+        parser.add_argument("--health-check-enabled", type=str2bool, default=False)
+        parser.add_argument("--health-check-idle-seconds", type=float, default=120.0)
+
+    def __init__(self, health_check_enabled=False, health_check_idle_seconds=120.0, hostname='0.0.0.0', port=8080, **kwargs):
+        self.health_check_enabled = health_check_enabled
+        self.health_check_idle_seconds = health_check_idle_seconds
+        self.hostname = hostname
+        self.port = port
+        self.idle_detector = IdleDetector(self.health_check_idle_seconds)
+        self.start()
+
+    def start(self):
+        if self.health_check_enabled:
+            HealthCheckHttpHandler.idle_detector = self.idle_detector
+            httpd = HTTPServer((self.hostname, self.port), HealthCheckHttpHandler)
+            thread = Thread(target=self.serve_forever, args=(httpd, ))
+            # flag the http server thread as daemon thread so that it can be abruptly stopped at shutdown
+            thread.setDaemon(True)
+            thread.start()
+            logging.info('Health check server is listening on %s:%d' % (self.hostname, self.port))
+
+    def add_probe(self, pipeline, element_name, pad_name):
+        if self.health_check_enabled:
+            element = pipeline.get_by_name(element_name)
+            if not element:
+                raise Exception("Unable to get element %s" % element_name)
+            sinkpad = element.get_static_pad(pad_name)
+            if not sinkpad:
+                raise Exception("Unable to get %s pad of %s" % (pad_name, element_name))
+            sinkpad.add_probe(Gst.PadProbeType.BUFFER, self.buffer_probe, self.idle_detector)
+
+    def serve_forever(self, httpd):
+        with httpd:  # to make sure httpd.server_close is called
+            httpd.serve_forever()
+
+    @staticmethod
+    def buffer_probe(pad, info, data):
+        gst_buffer = info.get_buffer()
+        if gst_buffer:
+            data.update()
+            logging.debug("buffer_timestamp_probe: %20s:%-8s: %s" % (
+                pad.get_parent_element().name,
+                pad.name,
+                data.to_string()))
+        return Gst.PadProbeReturn.OK
+
+
+class IdleDetector():
+    def __init__(self, tolerance):
+        self.update_at = time.monotonic() - tolerance
+        self.idle_time = 0
+        self.update_tolerance = tolerance
+
+    def update(self):
+        self.update_at = time.monotonic()
+
+    def to_string(self):
+        return "last update at %u seconds of the monotonic clock" % (self.update_at)
+
+    def is_healthy(self):
+        self.idle_time = time.monotonic() - self.update_at
+        return self.idle_time < self.update_tolerance
+
+
+class HealthCheckHttpHandler(BaseHTTPRequestHandler):
+    idle_detector = None
+
+    def send_code_msg(self, code, msg):
+        self.send_response(code)
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(msg.encode("utf-8"))
+
+    # Any code greater than or equal to 200 and less than 400 indicates success. Any other code indicates failure
+    # https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/
+    def do_GET(self):
+        if self.path == "/ishealthy":
+            if HealthCheckHttpHandler.idle_detector is None:
+                self.send_code_msg(500, "No detector is set")
+            elif HealthCheckHttpHandler.idle_detector.is_healthy():
+                self.send_code_msg(200, "OK")
+            else:
+                self.send_code_msg(500, "Pipeline has been idle for %d seconds" % (HealthCheckHttpHandler.idle_detector.idle_time))
+        else:
+            self.send_code_msg(404, "Not Found")
---
+++ python_apps/lib/gstpravega/util.py
@@ -0,0 +1,134 @@
+#
+# Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
+import ctypes
+import datetime
+import distutils.util
+import logging
+import gi
+from gi.repository import GObject, Gst
+
+
+def add_probe(pipeline, element_name, callback, pad_name="sink", probe_type=Gst.PadProbeType.BUFFER):
+    logging.info("add_probe: Adding probe to %s pad of %s" % (pad_name, element_name))
+    element = pipeline.get_by_name(element_name)
+    if not element:
+        raise Exception("Unable to get element %s" % element_name)
+    sinkpad = element.get_static_pad(pad_name)
+    if not sinkpad:
+        raise Exception("Unable to get %s pad of %s" % (pad_name, element_name))
+    sinkpad.add_probe(probe_type, callback, 0)
+
+
+def bus_call(bus, message, loop):
+    """Callback for GStreamer bus messages"""
+    t = message.type
+    if t == Gst.MessageType.EOS:
+        logging.info("End-of-stream")
+        loop.quit()
+    elif t == Gst.MessageType.WARNING:
+        err, debug = message.parse_warning()
+        logging.warning("%s: %s" % (err, debug))
+    elif t == Gst.MessageType.ERROR:
+        err, debug = message.parse_error()
+        logging.error("%s: %s" % (err, debug))
+        loop.quit()
+    elif t == Gst.MessageType.ELEMENT:
+        details = message.get_structure().to_string()
+        logging.info("%s: %s" % (message.src.name, str(details),))
+    elif t == Gst.MessageType.PROPERTY_NOTIFY:
+        details = message.get_structure().to_string()
+        logging.debug("%s: %s" % (message.src.name, str(details),))
+    return True
+
+
+def format_clock_time(ns):
+    """Format time in nanoseconds like 01:45:35.975000000"""
+    s, ns = divmod(ns, 1000000000)
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+    return "%u:%02u:%02u.%09u" % (h, m, s, ns)
+
+
+def glist_iterator(li):
+    """Iterator for a pyds.GLib object"""
+    while li is not None:
+        yield li.data
+        li = li.next
+
+
+def long_to_int(l):
+    value = ctypes.c_int(l & 0xffffffff).value
+    return value
+
+
+def make_element(factory_name, element_name):
+    """Create a GStreamer element, raising an exception on failure."""
+    logging.info("Creating element %s of type %s" % (element_name, factory_name))
+    element = Gst.ElementFactory.make(factory_name, element_name)
+    if not element:
+        raise Exception("Unable to create element %s of type %s" % (element_name, factory_name))
+    return element
+
+
+def resolve_pravega_stream(stream_name, default_scope):
+    if stream_name:
+        if "/" in stream_name:
+            return stream_name
+        else:
+            if not default_scope:
+                raise Exception("Stream %s given without a scope but pravega-scope has not been provided" % stream_name)
+            return "%s/%s" % (default_scope, stream_name)
+    else:
+        return None
+
+def str2bool(v):
+    return bool(distutils.util.strtobool(v))
+
+
+class PravegaTimestamp():
+    """This is a Python version of PravegaTimestamp in pravega-video/src/timestamp.rs."""
+
+    # Difference between NTP and Unix epochs.
+    # Equals 70 years plus 17 leap days.
+    # See [https://stackoverflow.com/a/29138806/5890553].
+    UNIX_TO_NTP_SECONDS = (70 * 365 + 17) * 24 * 60 * 60
+
+    # UTC to TAI offset.
+    # Below is valid for dates between 2017-01-01 and the next leap second.
+    # TODO: Beyond this range, we must use a table that incorporates the leap second schedule.
+    # See [https://en.wikipedia.org/wiki/International_Atomic_Time].
+    UTC_TO_TAI_SECONDS = 37
+
+    def __init__(self, nanoseconds):
+        self._nanoseconds = nanoseconds
+
+    def from_nanoseconds(nanoseconds):
+        """Create a PravegaTimestamp from the number of nanoseconds since the TAI epoch 1970-01-01 00:00:00 TAI."""
+        return PravegaTimestamp(nanoseconds)
+
+    def nanoseconds(self):
+        return self._nanoseconds
+
+    def to_unix_nanoseconds(self):
+        return self.nanoseconds() - self.UTC_TO_TAI_SECONDS * 1000*1000*1000
+
+    def to_unix_seconds(self):
+        return self.to_unix_nanoseconds() * 1e-9
+
+    def to_iso_8601(self):
+        seconds = self.to_unix_seconds()
+        return datetime.datetime.fromtimestamp(seconds, datetime.timezone.utc).isoformat()
+
+    def is_valid(self):
+        return self.nanoseconds() > 0
+
+    def __repr__(self):
+        return "%s (%d ns)" % (self.to_iso_8601(), self.nanoseconds())
